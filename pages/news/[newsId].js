745401eb9b806d1a334f6de7017262e3d33a8d1c
Alex Ungureanu <alex.ungureanu@cipherpoint.com>
2022-01-04 14:27:09 +0200

    initial push;created app

---
+++ pages/news/[newsId].js
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router'
+
+//our-domain.com/news/{newsId}
+function DetailPage() {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+
+
+    return <h1>The News Page</h1>
+}
+
+//https://www.youtube.com/watch?v=MFuwkrseXVE min: 43:29
+
+export default DetailPage;
